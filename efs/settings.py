60a8d492dfae838220fc1311c12d7ac60f933ad7
kavyaraviprakash <70050485+kavyaraviprakash@users.noreply.github.com>
2021-07-15 19:01:21 +0530

    Adding Media Root

--- efs/settings.py
+++ efs/settings.py
@@ -130,6 +130,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL ='/media/'
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
